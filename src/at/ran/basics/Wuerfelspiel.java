d33d82fe71f1717eea7d46f7e0a035e4e1041c64
Joppan420 <jovan.rakic@student.htldornbinr.at>
2021-11-24 09:33:40 +0100

    Würfelspiel

--- src/at/ran/basics/Wuerfelspiel.java
+++ src/at/ran/basics/Wuerfelspiel.java
@@ -1,47 +1,41 @@
 package at.ran.basics;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Wuerfelspiel {
     public static void main(String[] args) {
 
         boolean isFinished = false;
-        int total = 0;
-
-
-
-        Random random = new Random();
 
         while (!isFinished) {
-            printMenu();
-            int selection = random.nextInt();
-            System.out.println(selection);
 
             int a = 0;
             int b = 0;
 
+            System.out.println("--- 1 Spiel starten ---");
+            System.out.println("--- 2 Abbrechen ---");
+            Scanner scanner = new Scanner(System.in);
+            int selection = scanner.nextInt();
 
             if (selection == 1) {
-                System.out.println("Du hast eine" + a + "gewürfelt." + "Dein Gegner hat eine" + b + "gewürfelt = Gleichstand");
-                Random random1 = new Random();
-                double selection1 = random.nextDouble();
-                System.out.println(selection1);
-                total += selection1;
-            } else if (selection == 2) {
-                System.out.println("Du hast eine" + a + "gewürfelt." + "Dein Gegner hat eine" + b + "gewürfelt = Dein Gegner gewinnt");
-                Random random2 = new Random();
-                double selection2 = random.nextDouble();
-                System.out.println(selection2);
-                total -= selection2;
-            } else {
+                Random random = new Random();
+                for (int i = 0; i < 1; i++) {
+                    int randomNumberA = 1 + random.nextInt(6);
+                    int randomNumberB = 1 + random.nextInt(6);
+                    a += randomNumberA;
+                    b += randomNumberB;
+                }
             }
-
+            if (a > b) {
+                System.out.println("Du hast leider verloren. Der score vom Computer war " + a + ", und dein score war" + b);
+            } else if (a < b) {
+                System.out.println("Toll du hast gewonnen. Der score vom Computer war " + a + ", und dein score war" + b);
+            } else {
+                System.out.println("--- Wird geschlossen ---");
+                isFinished = true;
+            }
         }
-    }
-
-    public static void printMenu(){
-        System.out.println(" 1. Spiel starten");
-        System.out.println(" 2. Abbrechen");
     }
 }
 

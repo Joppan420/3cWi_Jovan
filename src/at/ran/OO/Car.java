b5b191edf1b8400b6b1671bc16d4f362f82fafe1
Joppan420 <jovan.rakic@student.htldornbinr.at>
2022-01-26 09:31:43 +0100

    Complete

--- src/at/ran/OO/Car.java
+++ src/at/ran/OO/Car.java
@@ -1,2 +1,17 @@
-package at.ran.OO;public class Car {
+package at.ran.OO;
+
+public class Car {
+
+    public int fuelConsumption;
+    public int fuelAmount;
+    public String brand;
+    public String serialNumber;
+    public String color;
+
+    //Methode
+    public void drive(){
+        this.fuelAmount =this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving");
+    }
+
 }
--- src/at/ran/OO/Main.java
+++ src/at/ran/OO/Main.java
@@ -1,2 +1,28 @@
-package at.ran.OO;public class Main {
+package at.ran.OO;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        c1.brand = "BMW";
+        c1.fuelConsumption = 8;
+        c1.serialNumber = "B530";
+        c1.color = "black";
+        c1.fuelAmount = 70;
+
+
+
+        Car c2 = new Car();
+        c2.brand = "Mercedes";
+        c2.fuelConsumption = 6;
+        c2.serialNumber = "M4881";
+        c2.color = "white";
+        c2.fuelAmount = 20;
+
+        System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
+
+    }
+
 }

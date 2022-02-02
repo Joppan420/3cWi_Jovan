5fa79007f99fa375cc52c44835393e7fb83bdaf5
Joppan420 <jovan.rakic@student.htldornbinr.at>
2022-02-02 09:33:35 +0100

    expend car

--- src/at/ran/OO/Car.java
+++ src/at/ran/OO/Car.java
@@ -7,11 +7,36 @@ public class Car {
     public String brand;
     public String serialNumber;
     public String color;
+    public int drivenRoute;
+    public int tankVolume;
 
     //Methode
     public void drive(){
-        this.fuelAmount =this.fuelAmount - fuelConsumption;
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
         System.out.println("I am driving");
+    }
+
+    public void dbreak(){
+        if(this.drivenRoute > 10);
+        System.out.println("I slow down");
+    }
+
+    public void turboBoost(){
+        if(this.tankVolume * 0.1 < fuelAmount){
+            System.out.println("SuperBoostMode");
+        }else {
+            System.out.println("Not enough fuel to go SuperBoost");
+        }
+    }
+
+    public void honk(int amountOfRepetitions){
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.println("Tuuut");
+        }
+    }
+
+    public void getRemainingRange(){
+        System.out.println(this.tankVolume - this.fuelConsumption + " Liter you have to drive");
     }
 
 }

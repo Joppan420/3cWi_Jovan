5bcaa3be954361e0e6af4b8dbcb943863144dbdb
Joppan420 <jovan.rakic@student.htldornbinr.at>
2022-04-08 11:51:17 +0200

    NewCars

--- src/at/ran/OO/CarsNew/Car.java
+++ src/at/ran/OO/CarsNew/Car.java
@@ -8,22 +8,39 @@ public class Car {
     private double basicConsumption;
     private Engine engine;
     private Producer producer;
+    private double drivenKilometers;
 
-    public Car(String color, int fullSpeed, int basePrice, double basicConsumption, Engine engine, Producer producer) {
+    public Car(String color, int fullSpeed, int basePrice, double basicConsumption, Engine engine, Producer producer, double drivenKilometers) {
         this.color = color;
         this.fullSpeed = fullSpeed;
         this.basePrice = basePrice;
         this.basicConsumption = basicConsumption;
         this.engine = engine;
         this.producer = producer;
+        this.drivenKilometers = drivenKilometers;
     }
 
     public void price() {
         double rabatt = this.producer.getRabatt();
         double price = basePrice - (basePrice * rabatt);
         System.out.println("Preis mit Rabatt = " + price);
-        System.out.println("Preis ohen Rabatt = " + basePrice);
+        System.out.println("Preis ohne Rabatt = " + basePrice);
     }
+
+    public void fueConsumption(){
+        if(drivenKilometers > 50000) {
+            double consumtionPrecent = 1.098;
+            setBasicConsumption((int) (basicConsumption * consumtionPrecent));
+
+            double newFuelConsumption1 = getBasicConsumption();
+            System.out.println(newFuelConsumption1);
+        }else{
+            double newFuelConsumption2 = getBasicConsumption();
+            System.out.println(newFuelConsumption2);
+        }
+    }
+
+
 
     public String getColor() {
         return color;
@@ -57,4 +74,11 @@ public class Car {
         this.basicConsumption = basicConsumption;
     }
 
+    public double getDrivenKilometers() {
+        return drivenKilometers;
+    }
+
+    public void setDrivenKilometers(double drivenKilometers) {
+        this.drivenKilometers = drivenKilometers;
+    }
 }
--- src/at/ran/OO/CarsNew/Main.java
+++ src/at/ran/OO/CarsNew/Main.java
@@ -3,5 +3,12 @@ package at.ran.OO.CarsNew;
 public class Main {
     public static void main(String[] args) {
 
+        Engine e1 = new Engine(5, Engine.TYPE.Diesel);
+        Producer p1 = new Producer("BMW","Germany",0.4);
+        Car c1 = new Car("black",280,110000, 8, e1,p1,30000 );
+
+        c1.price();
+        c1.fueConsumption();
+
     }
 }

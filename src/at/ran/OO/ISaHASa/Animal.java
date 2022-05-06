41bf540a7cb61245d48c0c7326e2b2ae76ea822e
Joppan420 <jovan.rakic@student.htldornbinr.at>
2022-05-06 11:41:16 +0200

    ISaHASa

---
+++ src/at/ran/OO/ISaHASa/Animal.java
@@ -0,0 +1,27 @@
+package at.ran.OO.ISaHASa;
+
+public class Animal {
+    private int weight;
+    private int height;
+
+    public Animal(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weigth) {
+        this.weight = weigth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
---
+++ src/at/ran/OO/ISaHASa/Main.java
@@ -0,0 +1,19 @@
+package at.ran.OO.ISaHASa;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Animal(40,20);
+        Animal a2 = new Animal(50,30);
+
+        Zoo zoo = new Zoo("Kinderzoo","Elefantstrasse");
+        zoo.addAnimal(a1);
+        zoo.addAnimal(a2);
+        zoo.printAnimals();
+
+        Dog d1 = new Dog(15,20);
+        zoo.addAnimal(d1);
+
+        zoo.printAnimals();
+
+    }
+}
